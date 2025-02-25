705293f888f0d98d0c3f21a18194f125a0259de4
VinhTran <vinhthanhtran03102004@gmail.com>
2025-02-25 08:49:54 +0700

    Try restarting

--- backend/EmployeeApp/views.py
+++ backend/EmployeeApp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from EmployeeApp.models import Departments, Employees
+from EmployeeApp.serializers import DepartmentSerializer, EmployeeSerializer
 
 # Create your views here.
+
+@csrf_exempt
+def departmentApi(request, id=0):
+    if request.method == 'GET':
+        departments = Departments.objects.all()
+        departments_serializer = DepartmentSerializer(departments, many=True)
+        return JsonResponse(departments_serializer.data, safe=False)
+    elif request.method == 'POST':
+        departments_data = JSONParser().parse(request)
+        departments_serializer = DepartmentSerializer(data=departments_data)
+        if departments_serializer.is_valid():
+            departments_serializer.save()
+            return JsonResponse("Added Successfully!", safe=False)
+        return JsonResponse("Failed to Add", safe=False)
+    elif request.method == 'PUT':
+        departments_data = JSONParser().parse(request)
+        departments = Departments.objects.get(DepartmentID = departments_data['DepartmentID'])
+        departments_serializer = DepartmentSerializer(departments, data=departments_data)
+        if departments_serializer.is_valid():
+            departments_serializer.save()
+            return JsonResponse("Update Successfully!", safe=False)
+        return JsonResponse("Failed to Update", safe=False)
+    elif request.method == 'DELETE':
+        departments = Departments.objects.get(DepartmentsID = id)
+        departments.delete()
+        return JsonResponse("Deleted Successfully!", safe=False)
